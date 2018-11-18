c9254c3cf5a6ce070fdcf9bf7c9c5c56cf475942
paul thibault <paulthb1@gmail.com>
2018-11-18 18:29:55 +0100

    début quadrillage

--- Virus/Assets/Script/GridManager.cs
+++ Virus/Assets/Script/GridManager.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField]
+    private GameObject tile;
+
+    public Tiles[][] Grid;
+
+    // Use this for initialization
+    void Start ()
+    {
+        SpawnGrid();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void SpawnGrid()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                GameObject temp = Instantiate(tile, new Vector2(i * 1, j * 1), Quaternion.identity);
+                Tiles theTile = temp.GetComponent<Tiles>();
+                if (i == 3 && j == 5)
+                    theTile.ChangeTypeTile(true);
+            }
+        }
+    }
 }
+
---
+++ Virus/Assets/Script/Tiles.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour {
+
+    private SpriteRenderer tilesImage;
+    bool startok = false;
+
+    [SerializeField]
+    private bool m_isWall = false;
+    //public bool IsWall
+    //{
+    //    get { return m_isWall; }
+    //    set
+    //    {
+    //        m_isWall = value;
+    //        if (m_isWall)
+    //            tilesImage.color = Color.blue;
+    //        else
+    //            tilesImage.color = Color.red;
+    //    }
+    //}
+
+    // Use this for initialization
+    void Start ()
+    {
+        startok = true;
+        tilesImage = GetComponent<SpriteRenderer>();
+        //if (m_isWall)
+        //    tilesImage.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ChangeTypeTile(bool isWall)
+    {
+        if (startok) Debug.Log("start ok"); else Debug.Log("start not ok");
+
+        m_isWall = isWall;
+
+        if (tilesImage == null)
+        {
+            Debug.Log("tilesImage == null");
+            tilesImage = GetComponent<SpriteRenderer>();
+        }
+        if (tilesImage != null)
+        {
+            if (m_isWall)
+                tilesImage.color = Color.blue;
+            else
+                tilesImage.color = Color.red;
+        }
+    }
+}
