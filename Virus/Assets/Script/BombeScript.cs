99ad552f3fbceeac0c464166c005dd1b506cd583
paul thibault <paulthb1@gmail.com>
2018-11-26 23:12:27 +0100

    Début bomb et interfaceManager
    création des préfabs et pas grand chose d'autre au final

---
+++ Virus/Assets/Script/BombeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Script;
+
+public class BombeScript : MonoBehaviour {
+
+    private int m_range = 1;
+    private int m_bombTimer = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Virus/Assets/Script/InterfaceManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Script;
+
+public class InterfaceManager : MonoBehaviour {
+
+    public GameObject Bomb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlaceBomb()
+    {
+        Instantiate(Bomb, GameManager.GetManager().m_currentPlayer.transform.position, Quaternion.identity);
+    }
+}
+////////////////garder le interfaceManager ou non ?? fin du tour a faire aussi
